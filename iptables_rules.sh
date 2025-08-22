36412ed2abb23ada1e3f5ce9454f8fe175603864
Cd8Zfq <138217024+Cd8Zfq@users.noreply.github.com>
2025-08-22 21:00:23 +0100

    Update iptables_rules.sh

--- iptables_rules.sh
+++ iptables_rules.sh
@@ -4,5 +4,5 @@ sudo iptables -I FORWARD -j NFQUEUE
 # rules for host layer only
 sudo iptables -I OUTPUT -j NFQUEUE
 sudo iptables -I INPUT -j NFQUEUE
-sudo iptables -I INPUT -p tcp --dport 22 -j ACCEPT
-sudo iptables -I OUTPUT -p tcp --sport 22 -j ACCEPT
+sudo iptables -I INPUT -p tcp --dport 22 -j ACCEPT #to let inbound ssh traffic pass
+sudo iptables -I OUTPUT -p tcp --sport 22 -j ACCEPT #to let outbound ssh traffic pass
